7c16d4c258d95e9326bb09c9d67e195e9a2cfc81
youpeng huang <CZ1CG1\youpeng.huang@a-1pxqvrynloxai.ad.openearth.community>
2020-02-12 17:19:23 -0600

    progressive web app demo

---
+++ build/precache-manifest.47d9fce205f0c6558f24ce776b7b17ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f1f3fc6053bf09c09711abced15b21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f21f39b00dcf6792a677",
+    "url": "/static/css/main.d5b5dc57.chunk.css"
+  },
+  {
+    "revision": "a6f4eacbed1afbe115b1",
+    "url": "/static/js/2.668e7150.chunk.js"
+  },
+  {
+    "revision": "1704bfdc64e4aee9dc0ff8e68c3a0ba0",
+    "url": "/static/js/2.668e7150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21f39b00dcf6792a677",
+    "url": "/static/js/main.72313f26.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
